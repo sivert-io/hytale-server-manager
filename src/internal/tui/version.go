fde3819b5f888b13aaa2b4977573f43d8876ee8e
Sivert Gullberg Hansen <sivertgullberg@gmail.com>
2026-01-17 22:29:30 -0600

    chore: release v0.1.1

--- src/internal/tui/version.go
+++ src/internal/tui/version.go
@@ -2,7 +2,7 @@ package tui
 
 // currentVersion is the current version of HSM (Hytale Server Manager)
 // This is updated automatically by the release script
-const currentVersion = "v0.1.0"
+const currentVersion = "v0.1.1"
 
 // GetVersion returns the current version string
 func GetVersion() string {
